11c22ecf67df6204cd3f3dd8c197a308e977b75e
VIVEK SATI <viveksati2002@gmail.com>
2023-05-09 05:32:21 +0530

    Added files

--- app/server.js
+++ app/server.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
   });
 
 app.get('/profile-picture', function (req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  let img = fs.readFileSync(path.join(__dirname, "/app/profile_picture.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
